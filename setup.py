1af483f4b6938b05633a2bb4310210532abacdcb
Yuwei Yan <yyan791@connect.hkust-gz.edu.cn>
2025-01-18 07:36:18 +0800

    ui update

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ BIN_SOURCES = {
         "darwin_arm64": "https://git.fiblab.net/api/v4/projects/195/packages/generic/socialcity-sim/v1.0.2/socialcity-sim-noproj-darwin-arm64",
     },
     "pycityagent-ui": {
-        "linux_x86_64": "https://git.fiblab.net/api/v4/projects/188/packages/generic/socialcity-web/v0.2.3/socialcity-web-linux-amd64",
-        "darwin_arm64": "https://git.fiblab.net/api/v4/projects/188/packages/generic/socialcity-web/v0.2.3/socialcity-web-darwin-arm64",
+        "linux_x86_64": "https://git.fiblab.net/api/v4/projects/188/packages/generic/socialcity-web/v0.3.0/socialcity-web-linux-amd64",
+        "darwin_arm64": "https://git.fiblab.net/api/v4/projects/188/packages/generic/socialcity-web/v0.3.0/socialcity-web-darwin-arm64",
     },
 }
 
